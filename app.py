84d70816e28ac69596e07b6944a35cdc2aa43860
haohuilu <95997507+haohuilu@users.noreply.github.com>
2023-10-14 21:18:58 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # Imports
 import dash
-from dash import dcc, html, callback_context
-from dash.dependencies import Input, Output, State
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
 from dash import dash_table
-from dash_table.Format import Format
 import dash_bootstrap_components as dbc
+from dash_dangerously_set_inner_html import DangerouslySetInnerHTML
 
 import pandas as pd
 import plotly.express as px
@@ -13,72 +14,55 @@ import string
 import numpy as np
 import nltk
 
-
-
 # Pre-processing functions
-def remove_urls(text):
-    new_text = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", text).split())
-    return new_text
 
 def text_lowercase(text):
     return text.lower()
 
-def remove_numbers(text):
-    result = re.sub(r'\d+', '', text)
-    return result
-
-def remove_punctuation(text):
-    translator = str.maketrans('', '', string.punctuation)
-    return text.translate(translator)
-
 def preprocessing(text):
-    text = text_lowercase(text)
-    text = remove_urls(text)
-    text = remove_numbers(text)
-    text = remove_punctuation(text)
-    return text
+    return text_lowercase(text)
 
 def jaccard_similarity(str1, str2):
     set1 = set(str1.lower().split())
     set2 = set(str2.lower().split())
-    intersection = len(set1.intersection(set2))
-    union = len(set1.union(set2))
-    return intersection / union
+    return len(set1.intersection(set2)) / len(set1.union(set2))
+
+def highlight_matching_words(sentence, input_words):
+    words = sentence.split()
+    highlighted_sentence = " ".join([f"<span style='background-color: yellow;'>{word}</span>" if word.lower() in input_words else word for word in words])
+    return highlighted_sentence
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
+
 app.layout = html.Div([
-    # Outermost Container
+    # Header
     html.Div([
-        # Header with Logo and Title
+        html.Img(src='/assets/1.png', id='logo', style={'height': '80px'}),
+        html.H2('BIT Textual Similarity Dashboard: A Jaccard Index Approach', style={'marginLeft': '20px'})
+    ], style={'display': 'flex', 'alignItems': 'center', 'background': '#f4f4f4', 'borderBottom': 'solid 1px #ddd', 'padding': '10px'}),
+    
+    # Main Content
+    html.Div([
+        # Dashboard 1
         html.Div([
-            html.Img(src='/assets/1.png', id='logo', style={'height': '80px'}),
-            html.H2('BIT Textual Similarity Dashboard: A Jaccard Index Approach', style={'marginLeft': '20px'})
-        ], style={'display': 'flex', 'alignItems': 'center', 'background': '#f4f4f4', 'borderBottom': 'solid 1px #ddd', 'padding': '10px'}),
-        
-        # Main Content Container
-        html.Div([
-            # Left Side
+            # Inputs for Dashboard 1
             html.Div([
-                html.Div(style={'height': '50px'}),  # Spacer
-                html.Label('Enter New Sentence:'),
+                html.Label('Enter Sentence:'),
                 dcc.Textarea(id='input-sentence', value='', style={'width': '100%', 'height': '100px', 'marginBottom': '20px'}),
-                
+                html.Button('Search', id='search-button', n_clicks=0),
+                html.Br(),  # New line for "Top N Similarities"
                 html.Label('Top N Similarities:'),
-                dcc.Input(id='input-top-n', value='5', type='number', style={'marginBottom': '20px'}),
-                
-                html.Label('Threshold (between 0 and 1):'),
-                dcc.Input(id='input-threshold', value='0.5', type='number', min=0, max=1, step=0.05, style={'marginBottom': '40px'}),
-                
-                html.Div(style={'height': '20px'}),  # <-- This is the spacer
-                html.Button('Search', id='search-button', n_clicks=0)
-            ], style={'width': '25%', 'padding': '40px', 'borderRight': 'solid 1px #ddd'}),
+                dcc.Input(id='input-top-n', value='5', type='number', style={'width': '100px', 'marginBottom': '20px'}),
+                html.Label('Threshold (between 0 and 1):', style={'marginRight': '10px'}),  # Add space
+                dcc.Input(id='input-threshold', value='0.5', type='number', min=0, max=1, step=0.05, style={'marginBottom': '40px'})
+            ], style={'width': '30%', 'padding': '20px', 'borderRight': 'solid 1px #ddd'}),
             
-            # Right Side
+            # Outputs for Dashboard 1
             html.Div([
                 html.Label('Similarity Table:'),
-                dcc.Loading(  # <-- Added this loading spinner component
+                dcc.Loading(
                     id="loading",
                     type="default",
                     children=[
@@ -86,9 +70,18 @@ app.layout = html.Div([
                         dcc.Graph(id='similarity-graph', style={'marginTop': '20px'})
                     ]
                 )
-            ], style={'width': '75%', 'padding': '20px'})
-        ], style={'display': 'flex', 'flexDirection': 'row'}),
-    ], style={'maxWidth': '1200px', 'margin': '0 auto'}),  # Centralize the content
+            ], style={'width': '70%', 'padding': '20px'})
+        ], style={'display': 'flex', 'flexDirection': 'row', 'borderBottom': 'solid 1px #ddd', 'padding': '10px'}),
+        
+        # Dashboard 2
+        html.Div([
+            # Outputs for Dashboard 2
+            html.Div([
+                html.Label('Most Similar Treaty:'),
+                html.Div(id='similarity-output', style={'marginTop': '20px'})
+            ], style={'padding': '20px'})
+        ]),
+    ], style={'maxWidth': '1200px', 'margin': '0 auto'}),
     
     # Footer
     html.Div([
@@ -96,49 +89,63 @@ app.layout = html.Div([
     ], style={'borderTop': '1px solid #ddd', 'background': '#f4f4f4'}),
 ])
 
+
+# Callbacks that use common input
 @app.callback(
     [Output('similarity-graph', 'figure'),
-     Output('similarity-table', 'data')],
+     Output('similarity-table', 'data'),
+     Output('similarity-output', 'children')],
     Input('search-button', 'n_clicks'),
     Input('input-sentence', 'value'),
     Input('input-top-n', 'value'),
     Input('input-threshold', 'value')
 )
-def update_graph(n_clicks, input_sentence, top_n, threshold):
+def update_outputs(n_clicks, input_sentence, top_n, threshold):
+    # For Dashboard 1
     if n_clicks == 0 or not input_sentence:
-        return px.scatter(), []
+        return px.scatter(), [], None
 
-    df = pd.read_csv("BIT.csv")
-    pp_text = [preprocessing(text) if isinstance(text, str) else np.NaN for text in df['treaty_article_sentence']]
-    df['pp_text'] = pp_text
+    df = pd.read_csv("BIT_all.csv")
+    df['pp_text'] = df['treaty_article_sentence'].apply(lambda x: preprocessing(x) if isinstance(x, str) else np.NaN)
+    similarities = df['pp_text'].apply(lambda x: jaccard_similarity(x, preprocessing(input_sentence)))
+    df['similarity'] = round(similarities,4)
 
-    new_sentence_pp = preprocessing(input_sentence)
-    new_row = pd.DataFrame({'pp_text': [new_sentence_pp]})
-    df = pd.concat([df, new_row], ignore_index=True)
+    most_similar_row = df.sort_values(by='similarity', ascending=False).iloc[0]
+    input_words = set(preprocessing(input_sentence).split())
+    highlighted_sentence = highlight_matching_words(most_similar_row['treaty_article_sentence'], input_words)
+    similarity_output = html.Div([
+        html.H5(f"Name: {most_similar_row['Name']}"),
+        html.P("Similar Sentence: ", style={'font-weight': 'bold'}),
+        DangerouslySetInnerHTML(highlighted_sentence)
+    ])
 
-    results = [
-        {
-            'BIT1': i, 
-            'BIT2': j, 
-            'similarity': round(jaccard_similarity(df['pp_text'][i], df['pp_text'][j]), 4)  # <-- Rounded to 4 decimal places
-        }
-        for i in df.index for j in range(i+1, len(df))
-    ]
-    sim_df = pd.DataFrame(results)
-    last_bit = max(sim_df['BIT2'].max(), sim_df['BIT1'].max())
-    last_bit_df = sim_df[(sim_df['BIT1'] == last_bit) | (sim_df['BIT2'] == last_bit)]
-    threshold_filtered = last_bit_df[last_bit_df['similarity'] >= float(threshold)]
+    # Create similarity table and graph as previously
+    threshold_filtered = df[df['similarity'] >= float(threshold)]
     top_n_similarity = threshold_filtered.sort_values(by='similarity', ascending=False).head(int(top_n))
-    merged_df = top_n_similarity.merge(df, left_on='BIT1', right_index=True)
-    merged_df['Date of signature'] = pd.to_datetime(merged_df['Date of signature'], dayfirst=True)
-    fig = px.scatter(merged_df, x='Date of signature', y='similarity', text='Name')
-    fig.update_traces(textposition='top center')
+    fig = px.scatter(top_n_similarity, x='Date of signature', y='similarity')
+    annotations = []
+    for index, row in top_n_similarity.iterrows():
+        annotations.append(
+            dict(
+                x=row['Date of signature'],
+                y=row['similarity'],
+                xref='x',
+                yref='y',
+                text=row['Name'],
+                showarrow=False,
+                font=dict(
+                    size=10
+                ),
+                ax=0,
+                ay=-40,
+                textangle=-45
+            )
+        )
+    fig.update_layout(annotations=annotations)
 
-    fig.update_layout(title='Timeline of BITs with Similarity')
+    table_data = top_n_similarity[["ID", "Name", "location", 'similarity']]
     
-    similarity_df = merged_df[["Name" ,'similarity']]
-    
-    return fig, similarity_df.to_dict('records')
+    return fig, table_data.to_dict('records'), similarity_output
 
 if __name__ == '__main__':
     app.run_server()
